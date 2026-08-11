5478e34a62b9afa3f5e0008a359763f26f3e46d5
Yukuan Hao <haogenmao@gmail.com>
2026-08-11 17:02:04 -0400

    chore: scaffold frontend (Vite + tailwind +_React + TS) AND backend(Express + TS)

---
+++ README.md
@@ -0,0 +1,13 @@
+# TROPHY WALL
+this project is a website to show a player's playing record just for fun to practice skills.
+
+## introduction
+### project structure
+
+## how to run
+
+## reference
+
+## things need to do
+
+   a commit hook for testing and linting
---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { log } from 'console';
+
+dotenv.config()
+
+const app = express();
+const PORT =  process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health',(_req, res)=> {
+    res.json({status: 'ok'});
+})
+
+app.listen(PORT, ()=> {
+    log('BACKEND RUNNING',PORT)
+});
